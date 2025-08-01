db9dfe8cd22bc2b8cfecb79d49d475a7cae3ab8a
Martial2023 <martialhilarionavadra@gmail.com>
2025-08-01 15:25:10 +0200

    Mis a jour 3

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,9 @@ export default function Home() {
 
   return (
     <main className='min-h-screen px-2 md:px-4'>
-      
+      <Link href="/sign-in" className='flex items-center justify-center h-screen text-2xl font-bold text-primary'>
+        Se connecter
+      </Link>
     </main>
   );
 }
